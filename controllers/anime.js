849d09771ae1238f629f015d1cd83e39f94d82e0
maliqtaylor <maliq.taylor@icloud.com>
2020-12-15 23:24:56 -0500

    responsive cards on index

--- controllers/anime.js
+++ controllers/anime.js
@@ -1,15 +1,14 @@
 const axios = require('axios');
-const { response } = require('express');
 
 module.exports = {
   index
 }
 
 function index(req, res) {
-  axios.get(`https://kitsu.io/api/edge/anime?page[limit]=9&page[offset]=0`)
-    .then((response) => {
-      console.log(response.data.data[0].attributes);
-      res.render("anime/index", { title: "Anime Index", user: req.user ? req.user : null, data: response.data.data });
+  axios.get(`https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0`)
+    .then((resp) => {
+      console.log(resp.data.data[0].attributes);
+      res.render("anime/index", { title: "Anime Index", user: req.user ? req.user : null, data: resp.data.data });
     })
 
 }
